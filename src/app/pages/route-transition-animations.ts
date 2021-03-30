75311369708b12e6585f9dfc1bfb89cd3f040e9f
Milan <puthukkuddymilan@gmail.com>
2021-03-30 09:49:40 +0530

    removed leave animation

--- src/app/pages/route-transition-animations.ts
+++ src/app/pages/route-transition-animations.ts
@@ -21,23 +21,23 @@ export const routeTransitionAnimations = trigger('triggerName', [
 			query(':enter', [animate('1s ease-out', style({ left: '0%', opacity: 1 }))])
 		]),
 		query(':enter', animateChild())
-	]),
-	transition('Two => One', [
-		style({ position: 'relative' }),
-		query(':enter, :leave', [
-			style({
-				position: 'absolute',
-				top: 0,
-				left: 0,
-				width: '100%'
-			})
-		]),
-		query(':enter', [style({ top: '-100%', opacity: 0 })]),
-		query(':leave', animateChild()),
-		group([
-			query(':leave', [animate('1s ease-out', style({ top: '100%', opacity: 0 }))]),
-			query(':enter', [animate('1s ease-out', style({ top: '0%', opacity: 1 }))])
-		]),
-		query(':enter', animateChild())
-	]),
+	])
+	// transition('Two => One', [
+	// 	style({ position: 'relative' }),
+	// 	query(':enter, :leave', [
+	// 		style({
+	// 			position: 'absolute',
+	// 			top: 0,
+	// 			left: 0,
+	// 			width: '100%'
+	// 		})
+	// 	]),
+	// 	query(':enter', [style({ top: '-100%', opacity: 0 })]),
+	// 	query(':leave', animateChild()),
+	// 	group([
+	// 		query(':leave', [animate('1s ease-out', style({ top: '100%', opacity: 0 }))]),
+	// 		query(':enter', [animate('1s ease-out', style({ top: '0%', opacity: 1 }))])
+	// 	]),
+	// 	query(':enter', animateChild())
+	// ]),
 ]);
